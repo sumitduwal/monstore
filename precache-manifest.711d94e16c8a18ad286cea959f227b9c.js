c66c3eced0aca4e98e8d4f612d96ac8eb32b5f0b
sumit <sandeep@triadsys.com>
2020-03-30 23:41:33 -0400

    Updates

---
+++ precache-manifest.711d94e16c8a18ad286cea959f227b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0157d19bb6f6e05b4a1ba24b43c961",
+    "url": "/monstore/index.html"
+  },
+  {
+    "revision": "8b6ed2ffc6aa7d3a15b5",
+    "url": "/monstore/static/css/main.5e281759.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monstore/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstore/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6ed2ffc6aa7d3a15b5",
+    "url": "/monstore/static/js/main.96ac55d0.chunk.js"
+  },
+  {
+    "revision": "1191ab371762d8d268ca",
+    "url": "/monstore/static/js/runtime-main.6e174fa9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monstore/static/media/logo.5d5d9eef.svg"
+  }
+]);
